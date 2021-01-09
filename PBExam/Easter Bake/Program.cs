cfb63acd44e8974bb505ab20aa3b72c6451c7bb8
chiborator999 <dsspasov@gmail.com>
2021-01-09 21:19:19 +0200

    add PBExam

---
+++ PBExam/Easter Bake/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Easter_Bake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numEasterCake = int.Parse(Console.ReadLine());
+            int sugarMax = int.MinValue;
+            int flourMax = int.MinValue;
+            int sugar = 0;
+            int flour = 0;
+            double totalSugar = 0;
+            double totalFlour = 0;
+
+            for (int i = 1; i <= numEasterCake; i++)
+            {
+                sugar = int.Parse(Console.ReadLine());
+                flour = int.Parse(Console.ReadLine());
+
+                sugarMax = Math.Max(sugar, sugarMax);
+                flourMax = Math.Max(flour, flourMax);
+
+                totalSugar += sugar;
+                totalFlour += flour;
+            }
+            totalSugar = Math.Ceiling(totalSugar / 950);
+            totalFlour = Math.Ceiling(totalFlour / 750);
+
+            Console.WriteLine($"Sugar: {totalSugar}");
+            Console.WriteLine($"Flour: {totalFlour}");
+            Console.WriteLine($"Max used flour is {flourMax} grams, max used sugar is {sugarMax} grams.");
+        }
+    }
+}
---
+++ PBExam/Easter Bakery/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Easter_Bakery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double flourPrice = double.Parse(Console.ReadLine());
+            double flourKg = double.Parse(Console.ReadLine());
+            double sugarKg = double.Parse(Console.ReadLine());
+            int eggsPlates = int.Parse(Console.ReadLine());
+            int bakerPowder = int.Parse(Console.ReadLine());
+
+            double priceSugar = (flourPrice * 0.75);
+            double priceSugarTotal = priceSugar * sugarKg;
+            double eggsPlatesPrice = (flourPrice * 1.1) * eggsPlates;
+            double bakerPowderPrice = (priceSugar * 0.2) * bakerPowder;
+
+            double price = priceSugarTotal + eggsPlatesPrice + bakerPowderPrice + (flourPrice * flourKg);
+            Console.WriteLine($"{price:F2}");
+        }
+    }
+}
