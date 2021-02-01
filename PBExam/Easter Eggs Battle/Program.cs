9f2c16793c70bb4dae2ec4977f6cd5c7fb9b9464
chiborator999 <dsspasov@gmail.com>
2021-02-01 23:29:12 +0200

    add new file

---
+++ PBExam/Easter Eggs Battle/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Easter_Eggs_Battle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerOne = int.Parse(Console.ReadLine());
+            int playerTwo = int.Parse(Console.ReadLine());
+            string input = string.Empty;
+            bool isZero = false;
+            while ((input = Console.ReadLine()) != "End of battle")
+            {
+                if (input == "one")
+                {
+                    playerTwo -= 1;
+                }
+                else if (input == "two")
+                {
+                    playerOne -= 1;
+                }
+
+                if (playerOne == 0 || playerTwo == 0)
+                {
+                    isZero = true;
+                    break;
+                }
+            }
+            if(isZero)
+            {
+                if (playerOne == 0)
+                {
+                    Console.WriteLine($"Player one is out of eggs. Player two has {playerTwo} eggs left.");
+                }
+                else if (playerTwo == 0)
+                {
+                    Console.WriteLine($"Player two is out of eggs. Player one has {playerOne} eggs left.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Player one has {playerOne} eggs left.");
+                Console.WriteLine($"Player two has {playerTwo} eggs left.");
+            }
+        }
+    }
+}
---
+++ PBExam/Easter Eggs/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace Easter_Eggs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numEggs = int.Parse(Console.ReadLine());
+            string color = string.Empty;
+            int redCount = 0;
+            int orangeCount = 0;
+            int blueCount = 0;
+            int greenCount = 0;
+
+            for (int i = 1; i <= numEggs; i++)
+            {
+                color = Console.ReadLine();
+                switch (color)
+                {
+                    case "red":
+                        redCount++;
+                        break;
+                    case "orange":
+                        orangeCount++;
+                        break;
+                    case "blue":
+                        blueCount++;
+                        break;
+                    case "green":
+                        greenCount++;
+                        break;
+                }
+            }
+
+            string[] nameColor = {"red", "orange", "blue", "green"};
+            int[] counter = {redCount, orangeCount, blueCount, greenCount};
+            int max = counter.Max();
+            int maxIndex = counter.ToList().IndexOf(max);
+
+//            $maxEggsCount = $redEggsCount;
+//            $maxColor = 'red';
+
+//            if ($orangeEggsCount > $maxEggsCount)
+//            {
+//                  $maxEggsCount = $orangeEggsCount;
+//                  $maxColor = 'orange';
+//            }
+//            if ($blueEggsCount > $maxEggsCount) 
+//            {
+//                  $maxEggsCount = $blueEggsCount;
+//                  $maxColor = 'blue';
+//            }
+//            if ($greenEggsCount > $maxEggsCount) 
+//            {
+//                  $maxEggsCount = $greenEggsCount;
+//                  $maxColor = 'green';
+//            }
+
+            Console.WriteLine($"Red eggs: {redCount}");
+            Console.WriteLine($"Orange eggs: {orangeCount}");
+            Console.WriteLine($"Blue eggs: {blueCount}");
+            Console.WriteLine($"Green eggs: {greenCount}");
+            Console.WriteLine($"Max eggs: {max} -> {nameColor[maxIndex]}");
+        }
+    }
+}
